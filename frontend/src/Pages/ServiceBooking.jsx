fa1de667fe02eac64bf15e14b79fe03538279e72
Abdul Moiz <abdulmoizfort@gmail.com>
2026-03-29 00:05:00 +0500

    Sync work and fix file collision

--- frontend/src/Pages/ServiceBooking.jsx
+++
@@ -1,9 +0,0 @@
-function ServiceBooking() {
-    return (
-        <div className="service-booking">
-            <h1>Service Booking</h1>
-        </div>
-    );
-}
-
-export default ServiceBooking;
