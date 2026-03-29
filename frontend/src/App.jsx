c07aca6c5230dd246dcaff75ddbe780a82fd893e
Abdul Moiz <abdulmoizfort@gmail.com>
2026-03-29 23:00:00 +0500

    Fix Bugs and irregularities

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import Login from './Pages/Login';
 import Register from './Pages/Register';
 import DashboardSwitcher from './Components/DashboardSwitcher';
 import ScheduleService from './Pages/ServiceBooking';
-import ServiceHistory from './Pages/ServiceHistory';
+import History from './Pages/History';
 import ServiceTracking from './Pages/ServiceTracking';
 import VehicleManagement from './Pages/VehicleManagement';
 
@@ -19,7 +19,7 @@ function MainApp(){
                 <Route path="/register" element={<Register />} />
                 <Route path="/dashboard" element={<DashboardSwitcher />} />
                 <Route path="/servicebooking" element={<ScheduleService />} />
-                <Route path="/servicehistory" element={<ServiceHistory />} />
+                <Route path="/servicehistory" element={<History />} />
                 <Route path="/servicetracking" element={<ServiceTracking />} />
                 <Route path="/vehiclemanagement" element={<VehicleManagement />} />
             </Routes>
--- frontend/src/Components/DashboardSwitcher.jsx
+++ frontend/src/Components/DashboardSwitcher.jsx
@@ -4,7 +4,7 @@ import ManagerDashboard from '../Pages/ManagerDashboard';
 import MechanicDashboard from '../Pages/MechanicDashboard';
 
 function DashboardSwitcher() {
-    const user = { role: 'manager' };
+    const user = { role: 'customer' };
     if (user.role === 'customer') return <CustomerDashboard />;
     if (user.role === 'manager') return <ManagerDashboard />;
     if (user.role === 'mechanic') return <MechanicDashboard />;
